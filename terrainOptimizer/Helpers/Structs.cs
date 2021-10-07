125a97e8d0e2e5c1d7d52866593d0a6646239855
mariuszhermansdorfer <49192999+mariuszhermansdorfer@users.noreply.github.com>
2021-10-07 14:46:43 +0200

    Added slope logic. Basic attractors for high/low points.

---
+++ terrainOptimizer/Helpers/Structs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace terrainOptimizer
+{
+    public class Structs
+    {
+        public class ControlPoints
+        {
+            public Anchors[] Anchors;
+            public bool[] Ascending;
+            public int Length;
+            public bool[] Pegged;
+            public Rhino.Geometry.Point3d[] Points;
+            
+
+            public ControlPoints(int length)
+            {
+                Anchors = new Anchors[length];
+                Ascending = new bool[length];
+                Length = length;
+                Pegged = new bool[length];
+                Points = new Rhino.Geometry.Point3d[length];
+            }
+        }
+        public enum Anchors
+        {
+            Null,
+            LowPoint,
+            HighPoint
+        }
+    }
+}
